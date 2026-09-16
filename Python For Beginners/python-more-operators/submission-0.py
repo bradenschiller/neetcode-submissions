2c0507ad9fa16103dae4de69498739148b6f5e66
Braden Schiller <26287414+bradenschiller@users.noreply.github.com>
2026-09-16 17:44:10 -0500

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 8, 5
+
+first_answer = a * b // c
+second_answer = a * b % c
+third_answer = a ** b
+forth_answer = b ** c
+
+print(first_answer)
+print(second_answer)
+print(third_answer)
+print(forth_answer)
