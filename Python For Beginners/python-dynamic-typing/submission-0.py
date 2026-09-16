4c60a47e00b6f1c965b0f8eafb4c6630527f5deb
Braden Schiller <26287414+bradenschiller@users.noreply.github.com>
2026-09-16 17:22:21 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+variable = 43
+
+print(type(variable))
+variable = 43.3
+
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = "Hello"
+
+print(type(variable))
+variable = [1, 2, 3]
+
+print(type(variable))
